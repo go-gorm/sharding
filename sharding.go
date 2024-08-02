472f994d22e0f874947c17425e6490bd0719a19f
Gowa2017 <33367355+Gowa2017@users.noreply.github.com>
2024-08-02 09:16:22 +0800

    fix: replace condition which with QualifyRef

--- sharding.go
+++ sharding.go
@@ -295,6 +295,20 @@ func (s *Sharding) switchConn(db *gorm.DB) {
 		s.mutex.Unlock()
 	}
 }
+func replaceConditionTableName(key, tableName string, expr sqlparser.Expr) error {
+
+	err := sqlparser.Walk(sqlparser.VisitFunc(func(node sqlparser.Node) error {
+		if n, ok := node.(*sqlparser.BinaryExpr); ok {
+			if q, ok2 := n.X.(*sqlparser.QualifiedRef); ok2 {
+				if q.Column.Name == key {
+					n.X.(*sqlparser.QualifiedRef).Table.Name = tableName
+				}
+			}
+		}
+		return nil
+	}), expr)
+	return err
+}
 
 // resolve split the old query to full table query and sharding table query
 func (s *Sharding) resolve(query string, args ...any) (ftQuery, stQuery, tableName string, err error) {
@@ -438,14 +452,17 @@ func (s *Sharding) resolve(query string, args ...any) (ftQuery, stQuery, tableNa
 			ftQuery = stmt.String()
 			stmt.FromItems = newTable
 			stmt.OrderBy = replaceOrderByTableName(stmt.OrderBy, tableName, newTable.Name.Name)
+			replaceConditionTableName(r.ShardingKey, newTable.Name.Name, stmt.Condition)
 			stQuery = stmt.String()
 		case *sqlparser.UpdateStatement:
 			ftQuery = stmt.String()
 			stmt.TableName = newTable
+			replaceConditionTableName(r.ShardingKey, newTable.Name.Name, stmt.Condition)
 			stQuery = stmt.String()
 		case *sqlparser.DeleteStatement:
 			ftQuery = stmt.String()
 			stmt.TableName = newTable
+			replaceConditionTableName(r.ShardingKey, newTable.Name.Name, stmt.Condition)
 			stQuery = stmt.String()
 		}
 	}
